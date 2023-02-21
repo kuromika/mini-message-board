8962aa6af1a4de37be2c86f8a31530a31c1cff68
mika-alpha <yuukeikioku@gmail.com>
2023-02-21 11:09:36 -0500

    Create message form & route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', messages: messages });
 });
 
+router.get('/new', function(req,res,next){
+  res.render('form');
+})
+
+router.post('/new', function(req,res, next){
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+  res.redirect('/');
+})
+
+
 module.exports = router;
